2f1c659aca0d103b73e4cb2238b3bab298a5a1f7
LWJ1982 <iambornin1982@gmail.com>
2024-02-08 22:46:59 +0800

    Terraform

--- provider.tf
+++ provider.tf
@@ -1,9 +1,13 @@
-terraform{
-    required_version = ">=1.6"
-    required_providers {
-      aws={
-        source = "hashicorp/aws"
-        version =  "~> 5.0"
-      }
+terraform {
+  required_version = ">=1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
+
+# provider "aws" {
+#   region  = "ap-southeast-1"
+# }
